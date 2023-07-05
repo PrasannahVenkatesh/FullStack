d12b9ef9464ec8da1fc15caa6c076676197fcdb1
HP <HP@Prasannah>
2023-07-05 11:04:13 +0530

    changes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,26 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
+
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { EmployeeListComponent } from './employee-list.component';
+import { EmployeeAddComponent } from './employee-add.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+	EmployeeListComponent,   
+	EmployeeAddComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
