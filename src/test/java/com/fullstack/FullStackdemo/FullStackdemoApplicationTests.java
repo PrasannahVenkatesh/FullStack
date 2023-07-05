8d7a485f881ef7f514ad664b9287d152ebf5e980
HP <HP@Prasannah>
2023-07-05 14:48:54 +0530

    test file

---
+++ src/test/java/com/fullstack/FullStackdemo/FullStackdemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fullstack.FullStackdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FullStackdemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
